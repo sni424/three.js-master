b7b43f5e433efd4564935352f605a103b51c7434
sni424 <jojonghyeok424@gmail.com>
2022-09-27 23:53:34 +0900

    광원에 관한 mesh

--- study/04-material.js
+++ study/04-material.js
@@ -13,6 +13,7 @@ class App {
 		/**랜더러 객체 setPixelRatio값 정의 */
 		renderer.setPixelRatio(window.devicePixelRatio);
 		divContainer.appendChild(renderer.domElement);
+
 		this._renderer = renderer;
 
 		/**scene객체 생성 */
@@ -36,18 +37,6 @@ class App {
 	_setupControls() {
 		new OrbitControls(this._camera, this._divCotainer);
 	}
-
-	_setupCamera() {
-		/**width,height 3차원 영역을 표현할 가로와 세로 크기 */
-		const width = this._divCotainer.clientWidth;
-		const height = this._divCotainer.clientHeight;
-		/**camera객체 생성 */
-		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-		camera.position.z = 2;
-		/**또다른 메서드에서 사용가능하도록  this._camera필드 객체로 설정*/
-		this._camera = camera;
-	}
-
 	_setupLight() {
 		/**광원 색상 */
 		const color = 0xffffff;
@@ -62,20 +51,90 @@ class App {
 	}
 	/**파랑색 개열의 정육면제를 생성하는 코드 */
 	_setupModel() {
-		const shape = new THREE.Shape();
-		shape.moveTo(1, 1);
-		shape.lineTo(1, -1);
-		shape.lineTo(1, -1);
-		shape.lineTo(1, -1);
-		shape.closePath();
+		const matarial = new THREE.MeshPhysicalMaterial({
+			color: 0xff0000,
+			emissive: 0x00000,
+			roughness: 0,
+			metalness: 0,
+			flatShading: false,
+			wireframe: false,
+			/**메쉬 코팅정도 */
+			clearcoat: 0,
+			/**코딩에 관한 거칠기값 */
+			clearcoatRoughness: 0,
+		});
+		// const matarial = new THREE.MeshStandardMaterial({
+		// 	color: 0xff0000,
+		// 	emissive: 0x00000,
+		// 	/**거칠기 광원에 대해 저항 */
+		// 	roughness: 0,
+		// 	/**금속성 쇠처럼 광원이 비춤 반사도됨 */
+		// 	metalness: 0,
+		// 	flatShading: false,
+		// 	wireframe: false,
+		// });
+		// /**MeshPhongMaterial 메쉬가 랜더링되는 픽셀단위로 광원을 계산 */
+		// const matarial = new THREE.MeshPhongMaterial({
+		// 	color: 0xff0000,
+		// 	emissive: 0x00000,
+		// 	/**광원에의해 반사되는 색상 광언을 비췄을때 */
+		// 	specular: 0x00000,
+		// 	/**위의 색상에대한 농도 0~10 */
+		// 	shiniess: 0,
+		// 	/**매쉬를 면으로 표시 */
+		// 	flatShading: false,
+		// 	wireframe: false,
+		// });
+		// /**MeshLambertMaterial메쉬를 구성하는 정점에서 광원을 계산 */
+		// const matarial = new THREE.MeshLambertMaterial({
+		// 	transparent: false,
+		// 	opacity: 1,
+		// 	side: THREE.FontSide,
+		// 	color: 0xff0000,
+		// 	/**광원에서 방출하는 값 기본 검은색은 어떤색도 방출 x */
+		// 	emissive: 0x00000,
+		// 	wireframe: false,
+		// });
+		// const matarial = new THREE.MeshBasicMaterial({
+		// 	/**랜더링시 메쉬가 보일지 안보일지 */
+		// 	visible: true,
+		// 	/**불투명도 opacity를 사용할지 말지 */
+		// 	transparent: false,
+		// 	opacity: 1,
+		// 	/**z-buffer 공부필요 */
+		// 	depthTest: true,
+		// 	depthWrite: true,
+		// 	/**mesh를 구성하는 면에서 앞면만 랜더링할지 뒤만 할지 모두 할지 */
+		// 	side: THREE.FontSide,
+		// 	color: 0xffff00,
+		// 	/**메쉬를 선형태로 랜더링할지 */
+		// 	wireframe: false,
+		// });
 
-		const geometry = new THREE.BufferGeometry();
-		const points = shape.getPoints();
-		geometry.setFromPoints(points);
+		const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), matarial);
+		box.position.set(-1, 0, 0);
+		this._scene.add(box);
 
-		const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
-		const line = new this._scene.add(group);
+		const sphere = new THREE.Mesh(
+			new THREE.SphereGeometry(0.7, 32, 32),
+			matarial
+		);
+		sphere.position.set(1, 0, 0);
+		this._scene.add(sphere);
 	}
+
+	_setupCamera() {
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			100
+		);
+		camera.position.z = 3;
+
+		this._camera = camera;
+	}
+
 	update(time) {
 		/**받은 time값에 0.001을 곱한다 */
 		time *= 0.001;
